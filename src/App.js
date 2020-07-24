10b83eb9750c9e85a136acc54624038e4b2e1a35
minabadrous <minabadrous.vu@gmail.com>
2020-07-25 01:42:05 +0200

    Added keyboard functionality from anywhere

--- src/App.js
+++ src/App.js
@@ -5,52 +5,62 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.playAudio = this.playAudio.bind(this);
+    this.handleKey = this.handleKey.bind(this);
   }
   playAudio(event){
     const ai = event.target.value;
     document.getElementById(ai).play();
   }
-  
-  render(){
+  handleKey(event){
+    if('qweasdzxcQWEASDZXC'.includes(event.key)){
+    document.getElementById(event.key.toUpperCase()).play();}
     
+  }
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKey, false);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.handleKey, false);
+  }
+  render(){
   return (
     <div className="App">
       <div id="drum-machine">
         <div id="display">
           <button onClick={this.playAudio} class="drum-pad" value="Q" id="qb">Q
-            <audio class="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3">
+            <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="W" id="wb">W
-            <audio class="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="W" id="wb">W
+            <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="E" id="eb">E
-            <audio class="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="E" id="eb">E
+            <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="A" id="ab">A
-            <audio class="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="A" id="ab">A
+            <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="S" id="sb">S
-            <audio class="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="S" id="sb">S
+            <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="D" id="db">D
-            <audio class="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="D" id="db">D
+            <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="Z" id="zb">Z
-            <audio class="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="Z" id="zb">Z
+            <audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="X" id="xb">X
-            <audio class="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="X" id="xb">X
+            <audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3">
             </audio>
           </button>
-          <button onClick={this.playAudio} class="drum-pad" value="C" id="cb">C
-            <audio class="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3">
+          <button onClick={this.playAudio} className="drum-pad" value="C" id="cb">C
+            <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3">
             </audio>
           </button>
         </div>
@@ -58,7 +68,7 @@ class App extends React.Component {
     </div>
     
   );}
-  
+ 
 }
 
 export default App;
